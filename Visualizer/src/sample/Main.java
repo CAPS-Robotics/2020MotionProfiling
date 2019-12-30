4d9fae1afda54183e0737d30c4c731f59c2b92a8
LAPTOP-9MBV8AE8\rnagp <rnagpal02@outlook.com>
2019-12-29 19:47:46 -0600

    Starting UI

--- Visualizer/src/sample/Main.java
+++ Visualizer/src/sample/Main.java
@@ -4,16 +4,29 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        setUI(primaryStage);
+    }
+
+    private void setUI(Stage stage) {
+        stage.setTitle("Pathfinding Visualizer");
+
+        // Make the stage take up the entire screen
+        stage.setHeight(Screen.getPrimary().getVisualBounds().getHeight());
+        stage.setWidth(Screen.getPrimary().getVisualBounds().getWidth());
+
+        VBox vBox = new VBox();
+
+        stage.setScene(new Scene(vBox, stage.getWidth(), stage.getHeight()));
+        stage.show();
     }
 
 
