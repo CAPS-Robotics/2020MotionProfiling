c57aa589c145efb718c66f4065f23b880d8dac3b
LAPTOP-9MBV8AE8\rnagp <rnagpal02@outlook.com>
2019-12-30 01:14:01 -0600

    Adding field background to graph

--- Visualizer/src/sample/HomeUI.java
+++ Visualizer/src/sample/HomeUI.java
@@ -12,32 +12,33 @@ public class HomeUI {
 
     public static void setUI(Stage stage) {
         stage.setTitle("Pathfinding Visualizer");
-
         stage.setHeight(Screen.getPrimary().getVisualBounds().getHeight());
         stage.setWidth(Screen.getPrimary().getVisualBounds().getWidth());
 
         VBox vBox = new VBox();
 
-        setGraph(1.5 * stage.getHeight(), 0.75 * stage.getHeight());
+        initializeGraph(1.38 * stage.getHeight(), 0.75 * stage.getHeight());
         vBox.getChildren().add(graph);
 
-        stage.setScene(new Scene(vBox, stage.getWidth(), stage.getHeight()));
+        Scene scene = new Scene(vBox, stage.getWidth(), stage.getHeight());
+        scene.getStylesheets().add("Styling/style.css");
+
+        stage.setScene(scene);
         stage.show();
     }
 
-    private static void setGraph(double width, double height) {
-        NumberAxis x = new NumberAxis(0, 54, 1);
-        x.setLabel("X");
-        NumberAxis y = new NumberAxis(0, 27, 1);
-        y.setLabel("Y");
+    private static void initializeGraph(double width, double height) {
+        NumberAxis x = new NumberAxis("X", 0, 54, 1);
+        NumberAxis y = new NumberAxis("Y",0, 27, 1);
 
         graph = new LineChart<>(x, y);
-
         graph.setMinWidth(width);
         graph.setMaxWidth(width);
+        graph.setPrefWidth(width);
         graph.setMinHeight(height);
         graph.setMaxHeight(height);
-
+        graph.setPrefHeight(height);
+        graph.setLegendVisible(false);
         graph.setTitle("Path");
     }
 }
--- Visualizer/src/sample/Main.java
+++ Visualizer/src/sample/Main.java
@@ -1,15 +1,7 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
-
-import static sample.HomeUI.setUI;
 
 public class Main extends Application {
 
