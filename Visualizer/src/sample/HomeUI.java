964daed1b7461b0e23a9cabb2d637233086dc16b
LAPTOP-9MBV8AE8\rnagp <rnagpal02@outlook.com>
2019-12-30 23:47:07 -0600

    Optimize graph for 2:1 aspect ratio

--- Visualizer/src/sample/HomeUI.java
+++ Visualizer/src/sample/HomeUI.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
@@ -16,9 +17,12 @@ public class HomeUI {
         stage.setWidth(Screen.getPrimary().getVisualBounds().getWidth());
 
         VBox vBox = new VBox();
+        vBox.setPadding(new Insets(10, 0, 0, 0));
 
-        initializeGraph(1.38 * stage.getHeight(), 0.75 * stage.getHeight());
-        vBox.getChildren().add(graph);
+        double graphHeight = 0.60 * stage.getHeight();
+        double graphWidth = 2 * graphHeight;
+
+        vBox.getChildren().add(initializeGraph(graphWidth, graphHeight));
 
         Scene scene = new Scene(vBox, stage.getWidth(), stage.getHeight());
         scene.getStylesheets().add("Styling/style.css");
@@ -27,9 +31,18 @@ public class HomeUI {
         stage.show();
     }
 
-    private static void initializeGraph(double width, double height) {
-        NumberAxis x = new NumberAxis("X", 0, 54, 1);
-        NumberAxis y = new NumberAxis("Y",0, 27, 1);
+    private static LineChart<Number, Number> initializeGraph(double width, double height) {
+        NumberAxis x = new NumberAxis("", 0, 54, 1);
+        double xHeight = 20;
+        x.setPrefHeight(xHeight);
+        x.setMinHeight(xHeight);
+        x.setMaxHeight(xHeight);
+
+        NumberAxis y = new NumberAxis("",0, 27, 1);
+        double yWidth = 40;
+        y.setPrefWidth(yWidth);
+        y.setMinWidth(yWidth);
+        y.setMaxWidth(yWidth);
 
         graph = new LineChart<>(x, y);
         graph.setMinWidth(width);
@@ -39,6 +52,7 @@ public class HomeUI {
         graph.setMaxHeight(height);
         graph.setPrefHeight(height);
         graph.setLegendVisible(false);
-        graph.setTitle("Path");
+
+        return graph;
     }
 }
